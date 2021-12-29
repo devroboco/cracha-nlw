102ebb54206156e8db62ce1fc552c0c0a638f2f7
Victor Dias Noleto <96757948+devroboco@users.noreply.github.com>
2021-12-28 21:24:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+    github: "devroboco",
+    youtube: "UCAFxSy46d-qPve-Xg8_oy6g",
+    facebook: "zzviczz",
+    instagram: "zzzzviczzzz",
+    twitter: "devroboco" 
+  }
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute('class')
+
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}` 
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+    const url = "https://api.github.com/users/devroboco"
+
+    fetch(url)
+    .then(response => response.json() )
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
